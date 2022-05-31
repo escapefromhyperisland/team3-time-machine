a25b9476b1f163b05e7b40fd969ea95b2e0928b9
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2022-05-31 21:27:08 +0200

    ground and sound

---
+++ Iwana/index.js
@@ -0,0 +1,36 @@
+AFRAME.registerComponent("scale-on-mouseclick", {
+  schema: {
+    to: {default: "0.5 0.5 0.5", type: "vec3"}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    this.el.addEventListener("click", function () {
+      el.object3D.scale.copy(data.to);
+    });
+  }
+});
+
+const background = new Audio("./assets/384713__ramonmineiro__mad-scientist-lab-loopable.mp3")
+background.volume = 0.2;
+background.loop = true;
+let musicIsPlaying = true;
+
+
+function handleMusic () {
+  const musicIcon = document.querySelector("#music-icon")
+    musicIsPlaying? background.pause() : background.play();
+  musicIsPlaying? musicIcon.className = "fas fa-volume-mute fa-xs" : musicIcon.className = "fas fa-volume-down fa-xs";
+    musicIsPlaying = !musicIsPlaying;
+}
+
+function closePopup() {
+    background.play()
+  document.querySelector("#popup").style.visibility = "hidden"
+  document.querySelector("#music-button").style.visibility = "visible"
+
+    setTimeout(()=>{
+      document.querySelector("#timetravel").style.visibility = "hidden";
+    })
+}
