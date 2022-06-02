3a57421396aae6f972ccd1d4c7c71044afdea50d
Anna Sakurai <zenasak@gmail.com>
2022-06-02 22:08:59 +0200

    fix button in challege 2

--- Anna/scripts/shinobi.js
+++ Anna/scripts/shinobi.js
@@ -2,10 +2,10 @@ const secondChallenge = document.querySelector(".second-challenge");
 const guessedWord = document.getElementById("unknown-letters");
 const secondChallengeInit = () => {
   secondChallenge.innerHTML += `
-  <img src="../assets/makimono02.png" alt="challenge 2 instruction"/>
+      <img src="../assets/makimono02.png" alt="challenge 2 instruction"/>
       <div id="input-btn-container">
         <input id="letter_input"></input>
-        <button id="guess_letter_btn" pys-onClick="on_click_letter">Click</button>
+        <button id="guess_letter_btn" class='enter-button' pys-onClick="on_click_letter">入力</button>
       </div>
       <div id="unknown-letters"></div>
       <div id='key'></div>
