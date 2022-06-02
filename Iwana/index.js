76b8c88b91fa12dbe41a3cb0cbd835e3e434c93e
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2022-06-02 10:58:44 +0200

    clue 2 positioned and eventlistener in place

--- Iwana/index.js
+++ Iwana/index.js
@@ -14,7 +14,9 @@ AFRAME.registerComponent("scale-down-on-mouseclick", {
 
 AFRAME.registerComponent("scale-up-on-mouseclick", {
   schema: {
-    to: {default: "3 3 3", type: "vec3"}
+    to: {default: "3 3 3", type: "vec3"},
+    radius: {type: "int", default: 4},
+    positiony: {default: .3, type: "number"}
   },
 
   init: function () {
@@ -22,6 +24,8 @@ AFRAME.registerComponent("scale-up-on-mouseclick", {
     var el = this.el;
     this.el.addEventListener("click", function () {
       el.object3D.scale.copy(data.to);
+      el.object3D.position.y = data.positiony;
+      // el.object3D.radius.set(data.radius);
     // here should the y-position been updatet to more than 0.16 (perhaps 0.55) 
     // perhaps with mouseenter, -leave?
     // and make the rotation -180 and the animation stop
