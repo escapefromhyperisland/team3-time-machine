ff01ccf76be5c7bf97351d083213df85b6fe0a21
Anna Sakurai <zenasak@gmail.com>
2022-06-04 21:57:09 +0200

    add 'click here' in the second challenge and clear the hint when 'click here' appears. chnage styles for #key

--- Anna/python/second_challenge.py
+++ Anna/python/second_challenge.py
@@ -5,9 +5,10 @@ import random
 
 words = ['sakura', 'kiku', 'botan', 'tsubaki', 'fuji', 'ume']
 
+hints = ['Japanese unofficial national flower which brings spring.', 'one of the national seals and a crest used by the Emperor of Japan and members of the Imperial Family.']
 
-
-chosen_word = words[random.randint(0, 3)]
+index = random.randint(0, 3)
+chosen_word = words[index]
 
 
 display = []
@@ -16,6 +17,8 @@ for x in range(len(chosen_word)):
 
 
 pyscript.write('unknown-letters', f"{' '.join(display)}")
+
+pyscript.write('hint', f"Hint: {hints[index]}")
 
 letter_input = Element('letter_input')
 
@@ -30,7 +33,9 @@ def on_click_letter(*args, **kwargs):
     correct = ''.join(display)
     if correct == chosen_word:
         print('you did it')
-        pyscript.write('key', "You did it! Change shinobi.html to sakura.html in the url.")
+        pyscript.write('hint', "")
+        pyscript.write('key', "You did it! Click HERE!")
+
 
     
     
--- Anna/scripts/shinobi.js
+++ Anna/scripts/shinobi.js
@@ -4,13 +4,18 @@ const secondChallengeInit = () => {
   secondChallenge.innerHTML += `
       <img src="../assets/makimono02.png" alt="challenge 2 instruction"/>
       <div id="input-btn-container">
-        <input id="letter_input"></input>
+        <input id="letter_input" placeholder="type in a letter"></input>
         <button id="guess_letter_btn" class='enter-button' pys-onClick="on_click_letter">入力</button>
       </div>
       <div id="unknown-letters"></div>
+      <p id='hint'></p>
       <div id='key'></div>
       <py-script src="../python/second_challenge.py"></py-script>
     `;
+
+  document.getElementById("key").addEventListener("click", () => {
+    window.location.href = "../pages/sakura.html";
+  });
 };
 
 secondChallengeInit();
