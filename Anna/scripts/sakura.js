459282ece4ddbb612f99e78a6dd18058cf480255
Anna Sakurai <zenasak@gmail.com>
2022-06-05 19:17:58 +0200

    add html elements for the modal and audio. add eventlistner for cho han buttons.

--- Anna/scripts/sakura.js
+++ Anna/scripts/sakura.js
@@ -1,6 +1,4 @@
 const thirdChallenge = document.querySelector(".third-challenge");
-const choBtn = document.querySelector("#cho");
-const hanBtn = document.querySelector("#han");
 
 const thirdChallengeInit = () => {
   thirdChallenge.innerHTML += `
@@ -13,25 +11,20 @@ const thirdChallengeInit = () => {
         </div>
 
         <div class="cho-han-btns-container">
-            <button class='chohanBtns' id="cho">丁</button>
-            <button class='chohanBtns' id="han">半</button>
+            <button class='chohanBtns' id="cho" pys-onClick="on_click_cho">丁</button>
+            <button class='chohanBtns' id="han" pys-onClick="on_click_han">半</button>
         </div>
+
+        <div id='modal' class='modal'>
+            <div class='modal-content'>
+             <div id='result'></div>
+            </div>
+        </div>
+        <div id='audio'></div>
     `;
 
-  choBtn.addEventListener(
-    "click",
-    (e) => {
-      btn.classList.toggle("pushed");
-    },
-    true
-  );
-  hanBtn.addEventListener(
-    "click",
-    (e) => {
-      btn.classList.toggle("pushed");
-    },
-    true
-  );
+  document.getElementById("cho").addEventListener("click", btnPressed);
+  document.getElementById("han").addEventListener("click", btnPressed);
 };
 
 thirdChallengeInit();
