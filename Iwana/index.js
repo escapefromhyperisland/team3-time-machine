fa4df762292f87eb6bc936733f56087dfb58514d
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2022-06-08 01:36:46 +0200

    not hardcoded function anymore for update codebox-text

--- Iwana/index.js
+++ Iwana/index.js
@@ -34,114 +34,48 @@ AFRAME.registerComponent("scale-up-on-mouseclick", {
   }
 });
 
+const elvira = (password, el) => {
+  let newPassword = "";
+  let indexNow = password.findIndex((x)=>(x === el.getAttribute('text').value ));
+  console.log("INDEXNOW FIRST: ", indexNow)
+  if (indexNow === -1 || indexNow === (password.length -1)){
+    newPassword = password[0]
+    console.log("NEW PASSWORD 0", newPassword )
+  }else {
+    newPassword = password[indexNow+1]
+    console.log("INDEXNOW +1", indexNow+1)
+    console.log("NEW PASSWORD", newPassword )
+  }
+  el.setAttribute("value", newPassword)
+}
 
 AFRAME.registerComponent("change-text-on-mouseclick", {
-  // schema: {
-  //   value: {default: "TECHNOLOGY, ART, PHILOSOPHY, GEOGRAPHY, PLAYFULNESS, WITCHERY", type: "array"},
-  // },
 
     update: function () {
       const password = ["ART", "TECHNOLOGY", "PHILOSOPHY", "GEOGRAPHY", "PLAYFULNESS", "WITCHERY"]
-      var data = this.data;
-      console.log("DATA: ", data)
       var el = this.el;
-
-
-      el.addEventListener('click', function (event) {
-
-        // const newWord = password.findIndex(el.getAttribute('text').value) === -1 ? password[0] : password.findIndex(el.getAttribute('text').value) >= password.length + 1 ? password 
-        if (el.getAttribute('text').value === "CODEBOX") {
-          el.setAttribute("value", password[0])
-        } else if (el.getAttribute('text').value === "ART") {
-          el.setAttribute("value", password[1])
-        } else if (el.getAttribute('text').value === "TECHNOLOGY") {
-          el.setAttribute("value", password[2])
-        } else if (el.getAttribute('text').value === "PHILOSOPHY") {
-          el.setAttribute("value", password[3])
-        } else if (el.getAttribute('text').value === "GEOGRAPHY") {
-          el.setAttribute("value", password[4])
-        } else if (el.getAttribute('text').value === "PLAYFULNESS") {
-          el.setAttribute("value", password[5])
-        } else if (el.getAttribute('text').value === "WITCHERY") {
-          el.setAttribute("value", password[0])
-        } 
-          // this.flushToDOM()
-      })
+      el.addEventListener('click', function (){elvira(password, el)})
   }
-
 });
 
 AFRAME.registerComponent("change-text1-on-mouseclick", {
-  // schema: {
-  //   value: {default: "TECHNOLOGY, ART, PHILOSOPHY, GEOGRAPHY, PLAYFULNESS, WITCHERY", type: "array"},
-  // },
 
     update: function () {
       const password = ["IS", "CAN", "DOES", "SAYS", "MEANS", "LETS"]
-      var data = this.data;
-      console.log("DATA: ", data)
       var el = this.el;
-
-
-      el.addEventListener('click', function (event) {
-
-        // const newWord = password.findIndex(el.getAttribute('text').value) === -1 ? password[0] : password.findIndex(el.getAttribute('text').value) >= password.length + 1 ? password 
-        if (el.getAttribute('text').value === "CODEBOX") {
-          el.setAttribute("value", password[0])
-        } else if (el.getAttribute('text').value === "IS") {
-          el.setAttribute("value", password[1])
-        } else if (el.getAttribute('text').value === "CAN") {
-          el.setAttribute("value", password[2])
-        } else if (el.getAttribute('text').value === "DOES") {
-          el.setAttribute("value", password[3])
-        } else if (el.getAttribute('text').value === "SAYS") {
-          el.setAttribute("value", password[4])
-        } else if (el.getAttribute('text').value === "MEANS") {
-          el.setAttribute("value", password[5])
-        } else if (el.getAttribute('text').value === "LETS") {
-          el.setAttribute("value", password[0])
-        } 
-          // this.flushToDOM()
-      })
+      el.addEventListener('click', function (){elvira(password, el)})
   }
-
 });
 
 AFRAME.registerComponent("change-text2-on-mouseclick", {
-  // schema: {
-  //   value: {default: "TECHNOLOGY, ART, PHILOSOPHY, GEOGRAPHY, PLAYFULNESS, WITCHERY", type: "array"},
-  // },
 
     update: function () {
       const password = ["NOTHING", "MOONSHINE", "SOMETHING", "THE WORLD", "EVERYTHING", "SUNSHINE"]
-      var data = this.data;
-      console.log("DATA: ", data)
-      var el = this.el;
-
-
-      el.addEventListener('click', function (event) {
-
-        // const newWord = password.findIndex(el.getAttribute('text').value) === -1 ? password[0] : password.findIndex(el.getAttribute('text').value) >= password.length + 1 ? password 
-        if (el.getAttribute('text').value === "CODEBOX") {
-          el.setAttribute("value", password[0])
-        } else if (el.getAttribute('text').value === "NOTHING") {
-          el.setAttribute("value", password[1])
-        } else if (el.getAttribute('text').value === "MOONSHINE") {
-          el.setAttribute("value", password[2])
-        } else if (el.getAttribute('text').value === "SOMETHING") {
-          el.setAttribute("value", password[3])
-        } else if (el.getAttribute('text').value === "THE WORLD") {
-          el.setAttribute("value", password[4])
-        } else if (el.getAttribute('text').value === "EVERYTHING") {
-          el.setAttribute("value", password[5])
-        } else if (el.getAttribute('text').value === "SUNSHINE") {
-          el.setAttribute("value", password[0])
-        } 
-          // this.flushToDOM()
-      })
+      let el = this.el;
+      el.addEventListener('click', function (){elvira(password, el)})
   }
-
 });
+
 
 AFRAME.registerComponent("text-visible-on-hover", {
   schema: {
@@ -149,13 +83,11 @@ AFRAME.registerComponent("text-visible-on-hover", {
   },
 
   init: function () {
-    var data = this.data;
-    var el = this.el;
-    console.log("Object3D: ", el.object3D.children[0].visible) 
-    // var defaultVisibility = el.getAttribute('visible').value;
+    let data = this.data;
+    let el = this.el;
     const defaultVisibility = el.object3D.children[0].visible
+
     el.addEventListener('mouseenter', function () {
-      // el.setAttribute('visible', data.visible);
       el.object3D.children[0].visible = data.visible
     });
 
@@ -164,26 +96,6 @@ AFRAME.registerComponent("text-visible-on-hover", {
     });
   }
 });
-
-// AFRAME.registerComponent("mouse-enter-text-visible", {
-//   schema: {
-//     to: {default: "true", type: "string"},
-//   },
-
-//   init: function () {
-//     var data = this.data;
-//     var el = this.el;
-//     this.el.addEventListener("mouseenter", function () {
-//       el.object3D.scale.copy(data.to);
-//       el.object3D.position.y = data.positiony;
-//       // el.object3D.radius.set(data.radius);
-//     // here should the y-position been updatet to more than 0.16 (perhaps 0.55) 
-//     // perhaps with mouseenter, -leave?
-//     // and make the rotation -180 and the animation stop
-//     // take away the changes when leaving??
-//     });
-//   }
-// });
 
 const background = new Audio("./assets/384713__ramonmineiro__mad-scientist-lab-loopable.mp3")
 background.volume = 0.2;
