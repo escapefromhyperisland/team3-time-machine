9ddb2b91cc8eabfb4427c497924ff6287a1b36da
Sophie <81225116+SophieKoenig@users.noreply.github.com>
2022-06-08 14:41:54 +0200

    mouse-maze is working

--- sophie/script/maze.js
+++ sophie/script/maze.js
@@ -1,8 +1,39 @@
-// var s = document.getElementById('container').style;
-// s.opacity = 0;
-// (function fade(){(s.opacity-=.1)<0?s.display="visible":setTimeout(fade,40)})();
+const nextButton = document.querySelector(".next-button");
+//svg mazes
+const levelOne = document.querySelector(".level-one");
+const levelTwo = document.querySelector(".level-two");
+//UI MESSAGES
+const uiLevel = document.querySelector(".ui-level");
+const uiMessage = document.querySelector(".ui-message");
+//End Game
+const spookyPicture = document.querySelector(".spooky-picture");
+const screamSound = document.querySelector(".scream-sound");
 
-// function changeColor() {
-//   document.body.style.background = "black";
-// }
-// changeColor();
+const collisionCheck = (value) => {
+  if (value === "maze-border") alert("You lost...try again.");
+  if (value === "finish") {
+    nextButton.style.opacity = 1;
+    nextButton.style.pointerEvents = "all";
+    levelOne.style.pointerEvents = "none";
+  }
+  if (value === "end-game") {
+    screamSound.play();
+    spookyPicture.style.display = "block";
+    document.body.style.background = "black";
+  }
+};
+
+window.addEventListener('mousemove', (e) => {
+  let check = e.target.classList.value;
+  console.log(e.target.classList.value);
+  collisionCheck(check);
+});
+
+// nextButton.addEventListener("click", () => {
+//   levelOne.style.display = "none";
+//   levelTwo.style.display = "block";
+//   nextButton.style.opacity = 0;
+//   nextButton.style.pointerEvents = "none";
+//   uiLevel.textContent = "Level 2";
+//   uiMessage.textContent = "One more to go...";
+// });
