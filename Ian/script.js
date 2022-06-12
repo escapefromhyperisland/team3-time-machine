0b58b68bca13f3cc5241f94df1b66b8b456c4d43
Ian Wallenberg <ianwallenberg@ians-mbp.home>
2022-06-12 14:28:02 +0200

    hacks issue solved

--- Ian/script.js
+++ Ian/script.js
@@ -2,18 +2,10 @@ const introVideo = document.getElementById("warp-overlay");
 
 const removeStartAnimation = () => {
     setTimeout(function () {
-      
-      document.getElementById("timetravel-video").remove();
       document.getElementById("warp-overlay").remove();
+      // document.getElementById("timetravel-video").remove();
+      
     }, 6000);
 }
 
 removeStartAnimation();
-
-// const shake = () => {
-//     setTimeout(function () {
-//       document.getElementsByClassName("body");
-//     }, 4000);
-// }
-
-// shake();
