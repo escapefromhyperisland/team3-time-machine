03e5190eb74b0421fc77f4e05c40d977901ae0c4
Sophie <81225116+SophieKoenig@users.noreply.github.com>
2022-06-13 18:53:25 +0200

    audio is now mp3, but not used

--- sophie/script/maze.js
+++ sophie/script/maze.js
@@ -5,18 +5,21 @@ const uiLevel = document.querySelector(".ui-level");
 const uiMessage = document.querySelector(".ui-message");
 //End Game
 // const spookyPicture = document.getElementById("spooky-picture");
-// const screamSound = document.querySelector(".scream-sound");
+//const screamSound = document.querySelector(".scream-sound");
 
 const collisionCheck = (value) => {
-  if (value === "maze-border") alert("You lost...try again.");
-  if (value === "finish") {
-    nextButton.style.opacity = 1;
-    nextButton.style.pointerEvents = "all";
-    levelOne.style.pointerEvents = "none";
+  if (value === "maze-border") {
+    alert("You lost...try again.");
+    //screamSound.play();
   }
+  // if (value === "finish") {
+  //   nextButton.style.opacity = 1;
+  //   nextButton.style.pointerEvents = "all";
+  //   levelOne.style.pointerEvents = "none";
+  // }
   if (value === "end-game") {
-    //alert("this is working");
-    window.parent.postMessage('nextLevel');
+    alert("this is working");
+    window.parent.postMessage("nextLevel");
     // screamSound.play();
 
     // spookyPicture.play();
