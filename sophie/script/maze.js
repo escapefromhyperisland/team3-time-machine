a8d423a23bfb8c8c97bf197c40082d0dbc6d12bb
Sophie <81225116+SophieKoenig@users.noreply.github.com>
2022-06-14 09:25:55 +0200

    cleaned out code

--- sophie/script/maze.js
+++ sophie/script/maze.js
@@ -1,30 +1,14 @@
 //svg mazes
 const levelTwo = document.querySelector(".level-two");
-//UI MESSAGES
-const uiLevel = document.querySelector(".ui-level");
-const uiMessage = document.querySelector(".ui-message");
-//End Game
-// const spookyPicture = document.getElementById("spooky-picture");
-//const screamSound = document.querySelector(".scream-sound");
+// const screamSound = document.querySelector(".scream-sound");
 
 const collisionCheck = (value) => {
   if (value === "maze-border") {
     alert("You lost...try again.");
-    //screamSound.play();
   }
-  // if (value === "finish") {
-  //   nextButton.style.opacity = 1;
-  //   nextButton.style.pointerEvents = "all";
-  //   levelOne.style.pointerEvents = "none";
-  // }
   if (value === "end-game") {
-    alert("this is working");
+    // alert("this is working");
     window.parent.postMessage("nextLevel");
-    // screamSound.play();
-
-    // spookyPicture.play();
-    // spookyPicture.style.display = "block";
-    // document.body.style.background = "black";
   }
 };
 
